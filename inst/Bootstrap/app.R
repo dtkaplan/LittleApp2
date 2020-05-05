180d86c70a4f59b7c277c9c9b7f04b8c81e31a6d
Danny Kaplan <kaplan@macalester.edu>
2020-05-05 14:14:15 -0500

    Deployed apps with Sullivan's data.

--- inst/Bootstrap/app.R
+++ inst/Bootstrap/app.R
@@ -1,4 +1,15 @@
 library(LittleApp2)
+library(shiny)
+library(shinyWidgets)
+library(shinyjs)
+library(miniUI)
+library(ggplot2)
+library(triola)
+library(sullystats6e)
+library(mosaicData)
+library(StatsUsingTechnologyData)
+library(openintro)
+library(Lock5Data)
 
 components <- LA_main(app_specifics = "Bootstrap_services.R",
                       covar = FALSE)
